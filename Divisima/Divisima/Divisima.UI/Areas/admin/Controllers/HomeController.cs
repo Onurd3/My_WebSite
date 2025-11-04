20540e3c5fb02910b8016a40efb52898ce20270b
Onur Ak <162779975+Onurd3@users.noreply.github.com>
2025-11-04 20:00:58 +0300

    Update HomeController.cs

--- Divisima/Divisima/Divisima.UI/Areas/admin/Controllers/HomeController.cs
+++ Divisima/Divisima/Divisima.UI/Areas/admin/Controllers/HomeController.cs
@@ -9,8 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Divisima.UI.Areas.admin.Controllers
 {
-	// [Area("admin")]
-	[Area("panel"), Authorize] // bu kısım daha sonra yazılır ve program.cs içerisinde ek servisler gerektirir
+	[Area("panel"), Authorize] 
 	public class HomeController : Controller
 	{
 		IRepository<Admin> repoAdmin;
@@ -23,23 +22,19 @@ namespace Divisima.UI.Areas.admin.Controllers
 			return View();
 		}
 
-		// [AllowAnonymous] // Anonim girişlere izin ver dedik, böylece kilitli olan bu controllerda bu kısım kilitli olmaktan çıktı
-		[AllowAnonymous, Route("admin/login")] // route ile bu sayfaya nasıl geleceğini anlattık
+		[AllowAnonymous, Route("admin/login")]
 		public IActionResult Login(string ReturnUrl)
 		{
 			ViewBag.ReturnUrl = ReturnUrl;
 			return View();
 		}
 
-
 		[AllowAnonymous, Route("admin/login"), HttpPost, ValidateAntiForgeryToken]
 		public async Task<IActionResult> Login(string username, string password, string ReturnUrl)
 		{
-			//...
-			//. .Net Core Custom Authentication
 			string md5Password = GeneralTools.GetMD5(password);
 			Admin admin = repoAdmin.GetBy(x => x.UserName == username && x.Password == md5Password);
-			if (admin != null) // MD5 metodundan sonra
+			if (admin != null)
 			{
 				List<Claim> claims = new List<Claim>
 				{
@@ -49,29 +44,19 @@ namespace Divisima.UI.Areas.admin.Controllers
 				ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "DivisimaAuth");
 				await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), new AuthenticationProperties() { IsPersistent = true });
 
-				// Bu derste eklendi
 				if (!string.IsNullOrEmpty(ReturnUrl)) return Redirect(ReturnUrl);
 				else return Redirect("/admin");
 			}
-			else TempData["bilgi"] = "Yanlış Kullanıcı Adı veya Parola";
-
-			// önce yaz
-			//if (repoAdmin.GetBy(x => x.UserName == username && x.Password == password) != null) // getMD5 metodunu git yaz. Çünkü kulanıcı normal değer girecek ama kayıt olurken şifresi md5 olarak kayıt oluyor.
-			//{
-
-			//}
-
+			else TempData["bilgi"] = "Yanlış Kullanıcı Adı veya Parola";	
 
 			return RedirectToAction("Login");
 		}
 
-		[Route("admin/logout")] // program.cs içerisinde yeri zaten hazır idi
+		[Route("admin/logout")]
 		public async Task<IActionResult> LogOut()
 		{
 			await HttpContext.SignOutAsync();
 			return Redirect("/");
 		}
-
-
 	}
 }
